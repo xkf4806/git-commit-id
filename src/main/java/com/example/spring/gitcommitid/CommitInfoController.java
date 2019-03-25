9a29a59a81afdeb34e53a3872aa95e6e96a63a55
Michael-X <1034524903@qq.com>
2019-03-25 11:41:11 +0800

    commit content-update,and make git not dirty

--- src/main/java/com/example/spring/gitcommitid/CommitInfoController.java
+++ src/main/java/com/example/spring/gitcommitid/CommitInfoController.java
@@ -1,11 +1,13 @@
 package com.example.spring.gitcommitid;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * @author xinj.x
@@ -21,12 +23,31 @@ public class CommitInfoController {
   @Value("${git.commit.id}")
   private String commitId;
 
-  @RequestMapping("/commitId")
-  public Map<String, String> getCommitId() {
-    Map<String, String> result = new HashMap<>();
-    result.put("Commit message",commitMessage);
-    result.put("Commit branch", branch);
-    result.put("Commit id", commitId);
-    return result;
+  @GetMapping(value = "/version")
+  public String versionInformation() {
+    return readGitProperties();
+  }
+
+  private String readGitProperties() {
+    ClassLoader classLoader = getClass().getClassLoader();
+    InputStream inputStream = classLoader.getResourceAsStream("git.properties");
+    try {
+      return readFromInputStream(inputStream);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return "Version information could not be retrieved";
+    }
+  }
+
+  private String readFromInputStream(InputStream inputStream) throws IOException {
+    StringBuilder resultStringBuilder = new StringBuilder();
+    try (BufferedReader br
+                 = new BufferedReader(new InputStreamReader(inputStream))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        resultStringBuilder.append(line).append("\n");
+      }
+    }
+    return resultStringBuilder.toString();
   }
 }
